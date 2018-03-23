78580cb0c5712fcb3b581923e1203b06ce69771c
Rafael312 <rafaelgomesdossantos5@hotmail.com>
2018-03-22 22:33:37 -0300

    atividade05

--- atividade05.py
+++ atividade05.py
@@ -114,3 +114,35 @@ elif prod2 < prod1:
 elif prod3 <= prod2 :
     print("O terceiro é mais barato.")
 #9-Faça um Programa que leia três números e mostre-os em ordem decrescente.
+
+n1 = input('Digite o 1° numero:')
+n1 = int(n1)
+n2 = input('Digite o 2° numero:')
+n2 = int(n2)
+n3 = input('Digite o 3° numero:')
+n3 = int(n3)
+
+if n1 > n2 and n3:
+    print("n1")
+elif n2 > n3 and n1:
+    print("n2")
+if n3 > n1 and n2:
+    print("n3")
+
+#agora o numero do meio
+if n1 > n3 and n2:
+	 print("n1")
+ elif n2 > n1 and n3:
+	  print("n2")
+   elif n3 > n2 and n1:
+    print("n3")
+elif n2 < n1 and n3:
+    print("n2")
+
+
+if n1 < n2 and n3:
+    print("n1")
+elif n2 < n3 and n1:
+    print("n2")
+elif n3 < n1 and n2:
+    print("n3")
